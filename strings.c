06d5834581dcc9fe7cb82dc21e2824cd70756731
root <richkibz@gmail.com>
2022-12-16 00:51:00 +0300

    stack and queus

---
+++ strings.c
@@ -0,0 +1,109 @@
+#include "monty.h"
+
+/**
+* f_pchar - function that prints the char at the top of the stack,
+* followed by a new line
+* @head: stack head
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_pchar(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	h = *head;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
+
+/**
+ * f_pstr - function that prints the string starting at the top of the stack,
+ * followed by a new
+ * @head: head of stack
+ * @counter: line count
+ *
+ * Return: nothing
+*/
+void f_pstr(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
+
+/**
+* f_queue - function that prints the top of the queue
+* @head: head of queue
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+* addqueue - function that add node to the tail the queue
+* @n: new value
+* @head: head of the queue
+*
+* Return: nothing
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (temp)
+	{
+		while (temp->next)
+			temp = temp->next;
+	}
+	if (!temp)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		temp->next = new_node;
+		new_node->prev = temp;
+	}
+}
